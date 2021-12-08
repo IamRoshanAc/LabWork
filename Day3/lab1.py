2c8b598e05940e3ae569a57c15b3f80896b6b64a
ImRoshanAc <roshanacharya6533@gmail.com>
2021-12-08 12:02:27 +0545

    Added  Day3

---
+++ Day3/lab1.py
@@ -0,0 +1,8 @@
+#check whether 5 is in list of first 5 natural numbers or not. Hint:List =>[1,2,3,4,5]
+i = [1,2,3,4,5]
+if(i == 5 ):
+    print("5 is in the list") 
+else:
+    print("5 is not in the list")
+  
+
---
+++ Day3/lab7.py
@@ -0,0 +1,4 @@
+import math
+realNumber = float(input("Enter a real number"))
+a, b = math.modf(realNumber)
+print(b)
